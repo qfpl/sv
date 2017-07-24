67f2527597074a0bacb94d73a2dbbb4b9672d08f
George Wilson <george@wils.online>
2017-07-24 12:27:35 +1000

    Test both kinds of quotedness

--- test/Data/CSV/ParserTest.hs
+++ test/Data/CSV/ParserTest.hs
@@ -1,8 +1,11 @@
+{-# language RankNTypes #-}
+
 module Data.CSV.ParserTest where
 
 import           Data.Either      (isLeft)
 import           Test.Tasty       (TestTree, testGroup)
 import           Test.Tasty.HUnit (assertBool, testCase, (@?=))
+import           Text.Parser.Char (CharParsing)
 import           Text.Parsec
 
 import           Data.CSV.Parser
@@ -11,20 +14,24 @@ test_Parser :: TestTree
 test_Parser =
   testGroup "Parser" [
     singleQuotedFieldTest
---  , doubleQuotedFieldTest
+  , doubleQuotedFieldTest
   ]
 
-singleQuotedFieldTest :: TestTree
-singleQuotedFieldTest =
-  let p = parse singleQuotedField ""
-  in testGroup "singleQuotedField" [
+quotedFieldTest :: (forall m . CharParsing m => m String) -> String -> String -> TestTree
+quotedFieldTest parser name q =
+  let p = parse parser "" . concat
+  in testGroup name [
     testCase "pass" $
-      p "'hello text'" @?= Right "hello text"
+      p [q,"hello text",q] @?= Right "hello text"
   , testCase "no closing quote" $
-      assertBool "wasn't left" (isLeft (p "'no closing quote"))
+      assertBool "wasn't left" (isLeft (p [q, "no closing quote"   ]))
   , testCase "no opening quote" $
-      assertBool "wasn't left" (isLeft (p "no opening quote'"))
+      assertBool "wasn't left" (isLeft (p [   "no opening quote", q]))
   , testCase "no quotes" $
-      assertBool "wasn't left" (isLeft (p "no quotes"))
+      assertBool "wasn't left" (isLeft (p [   "no quotes"          ]))
   ]
+
+singleQuotedFieldTest, doubleQuotedFieldTest :: TestTree
+singleQuotedFieldTest = quotedFieldTest singleQuotedField "singleQuotedField" "'"
+doubleQuotedFieldTest = quotedFieldTest doubleQuotedField "doubleQuotedField" "\""
 
