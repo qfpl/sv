6539b81116b3db705af121bf61fc5c4ae9272a23
George Wilson <george@wils.online>
2017-07-24 16:58:30 +1000

    Add more tests and fix bugs that tests caught.

--- src/Data/CSV/Parser.hs
+++ src/Data/CSV/Parser.hs
@@ -1,15 +1,22 @@
 module Data.CSV.Parser where
 
-import           Control.Applicative     ((<$>), (<|>))
+import           Control.Applicative     (Alternative, (<$>), (<|>))
 import           Data.CharSet            (CharSet)
 import qualified Data.CharSet as CharSet
-import           Text.Parser.Char        (CharParsing, char, notChar, noneOfSet, spaces, string)
-import           Text.Parser.Combinators (between, choice, many)
+import           Text.Parser.Char        (CharParsing, char, notChar, noneOfSet, oneOfSet, string)
+import           Text.Parser.Combinators (between, choice, many, sepBy, sepEndBy, sepEndBy1, some, try)
 
-singleQuote, doubleQuote, backslash :: Char
+singleQuote, doubleQuote, backslash, comma, pipe, tab :: Char
 singleQuote = '\''
 doubleQuote = '"'
 backslash = '\\'
+comma = ','
+pipe = '|'
+tab = '\t'
+
+concatMany, concatSome :: Alternative f => f [a] -> f [a]
+concatMany p = concat <$> many p
+concatSome p = concat <$> some p
 
 singleQuotedField, doubleQuotedField :: CharParsing m => m String
 singleQuotedField = quotedField singleQuote
@@ -18,7 +25,7 @@ doubleQuotedField = quotedField doubleQuote
 quotedField :: CharParsing m => Char -> m String
 quotedField quote =
   let cq = char quote
-  in spaced (between cq cq (escapeChar quote <|> many (notChar quote)))
+  in spaced (between cq cq (concatMany (escapeChar quote <|> fmap pure (notChar quote))))
 
 escapeChar :: CharParsing m => Char -> m String
 escapeChar c = string (concatenate backslash c)
@@ -28,7 +35,7 @@ concatenate c d = [c,d]
 
 unquotedField :: CharParsing m => Char -> m String
 unquotedField sep =
-  concat <$> many (
+  concatMany (
     escapeChar sep <|>
       (pure <$> noneOfSet (newlineOr sep))
   )
@@ -40,8 +47,19 @@ newlines :: CharSet
 newlines = CharSet.fromList "\r\n"
 
 field :: CharParsing m => Char -> m String
-field sep = choice [singleQuotedField, doubleQuotedField, unquotedField sep]
+field sep = choice [try singleQuotedField, try doubleQuotedField, unquotedField sep]
+
+horizontalSpace :: CharParsing m => m Char
+horizontalSpace = choice (fmap char [' ', '\t'])
 
 spaced :: CharParsing m => m a -> m a
-spaced = between spaces spaces
+spaced =
+  let s = many horizontalSpace
+  in between s s
+
+record :: CharParsing m => Char -> m [String]
+record sep = field sep `sepEndBy1` spaced (char sep)
+
+separatedValues :: CharParsing m => Char -> m [[String]]
+separatedValues sep = record sep `sepEndBy` some (oneOfSet newlines)
 
--- test/Data/CSV/ParserTest.hs
+++ test/Data/CSV/ParserTest.hs
@@ -4,7 +4,7 @@ module Data.CSV.ParserTest where
 
 import           Data.Either      (isLeft)
 import           Test.Tasty       (TestTree, testGroup)
-import           Test.Tasty.HUnit (assertBool, testCase, (@?=))
+import           Test.Tasty.HUnit (Assertion, assertBool, testCase, (@?=))
 import           Text.Parser.Char (CharParsing)
 import           Text.Parsec
 
@@ -15,14 +15,23 @@ test_Parser =
   testGroup "Parser" [
     singleQuotedFieldTest
   , doubleQuotedFieldTest
+  , fieldTest
+  , recordTest
   ]
+
+(@?=/) ::
+  (Show a, Show b, Eq a, Eq b)
+  => Either a b
+  -> b
+  -> Assertion
+(@?=/) l r = l @?= Right r
 
 quotedFieldTest :: (forall m . CharParsing m => m String) -> String -> String -> TestTree
 quotedFieldTest parser name q =
   let p = parse parser "" . concat
   in testGroup name [
     testCase "pass" $
-      p [q,"hello text",q] @?= Right "hello text"
+      p [q,"hello text",q] @?=/ "hello text"
   , testCase "no closing quote" $
       assertBool "wasn't left" (isLeft (p [q, "no closing quote"   ]))
   , testCase "no opening quote" $
@@ -34,4 +43,40 @@ quotedFieldTest parser name q =
 singleQuotedFieldTest, doubleQuotedFieldTest :: TestTree
 singleQuotedFieldTest = quotedFieldTest singleQuotedField "singleQuotedField" "'"
 doubleQuotedFieldTest = quotedFieldTest doubleQuotedField "doubleQuotedField" "\""
+
+fieldTest :: TestTree
+fieldTest =
+  let p = parse (field comma) ""
+  in  testGroup "field" [
+    testCase "doublequoted" $
+      p "\"hello\"" @?=/ "hello"
+  , testCase "singlequoted" $
+      p "'goodbye'" @?=/ "goodbye"
+  , testCase "unquoted" $
+      p "yes" @?=/ "yes"
+  , testCase "spaced doublequoted" $
+     p "       \" spaces \"    " @?=/ " spaces "
+  , testCase "spaced singlequoted" $
+     p "        ' more spaces ' " @?=/ " more spaces "
+  , testCase "spaced unquoted" $
+     p "  text  " @?=/ "  text  "
+  , testCase "fields can include the separator in single quotes" $
+     p "'hello,there,'" @?=/ "hello,there,"
+  , testCase "fields can include the separator in double quotes" $
+     p "\"court,of,the,,,,crimson,king\"" @?=/ "court,of,the,,,,crimson,king"
+  , testCase "unquoted fields stop at the separator (1)" $
+     p "close,to,the,edge" @?=/ "close"
+  , testCase "unquoted fields stop at the separator (2)" $
+     p ",close,to,the,edge" @?=/ ""
+  ]
+
+recordTest :: TestTree
+recordTest =
+  let p = parse (record comma) ""
+  in  testGroup "record" [
+    testCase "empty" $
+      p "" @?=/ []
+  , testCase "newlines" $
+      p "\n\n\r\n" @?=/ []
+  ]
 
