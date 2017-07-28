486ac0e562ea24f0544e4f847809730e67be9a97
Dave Laing <dave.laing.80@gmail.com>
2017-07-28 16:20:42 +1000

    Makes some changes to help with GHC 7.10.3

--- separated-values.nix
+++ separated-values.nix
@@ -1,13 +1,16 @@
 { mkDerivation, base, charset, parsec, parsers, semigroupoids
-, stdenv, tasty, tasty-discover, tasty-hunit, tasty-quickcheck
+, semigroups, stdenv, tasty, tasty-discover, tasty-hunit
+, tasty-quickcheck
 }:
 mkDerivation {
   pname = "separated-values";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base charset parsers semigroupoids ];
+  libraryHaskellDepends = [
+    base charset parsers semigroupoids semigroups
+  ];
   testHaskellDepends = [
-    base parsec parsers tasty tasty-discover tasty-hunit
+    base parsec parsers semigroups tasty tasty-discover tasty-hunit
     tasty-quickcheck
   ];
   homepage = "https://github.com/data61/separated-values";
