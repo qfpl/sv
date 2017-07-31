c520474753d3e9e5b4ba71ec70bbdc1b269e3c1e
George Wilson <george@wils.online>
2017-07-31 11:44:54 +1000

    Add dependency on semigroups
    Semigroups were not in base until GHC 8.0, so we add an explicit dependency
    on semigropus in order to build on 7.10.2.

--- separated-values.nix
+++ separated-values.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, charset, parsec, parsers, semigroupoids
+{ mkDerivation, base, charset, parsec, parsers, semigroupoids, semigroups
 , stdenv, tasty, tasty-discover, tasty-hunit, tasty-quickcheck
 }:
 mkDerivation {
   pname = "separated-values";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base charset parsers semigroupoids ];
+  libraryHaskellDepends = [ base charset parsers semigroupoids semigroups ];
   testHaskellDepends = [
     base parsec parsers tasty tasty-discover tasty-hunit
     tasty-quickcheck
