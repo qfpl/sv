9b84fee1dea6fd183dfc3477b01145ace5b5b260
George Wilson <george@wils.online>
2017-08-09 11:14:12 +1000

    Don't use tasty-discover. Travis doesn't seem to like it.

--- test/Tasty.hs
+++ test/Tasty.hs
@@ -1,1 +1,13 @@
-{-# OPTIONS_GHC -F -pgmF tasty-discover #-}
+import Test.Tasty
+
+import Data.CSV.ParserTest (test_Parser)
+import Data.CSV.PrettyTest (test_Pretty)
+
+
+main :: IO ()
+main =
+  defaultMain $ testGroup "Tests" [
+    test_Parser
+  , test_Pretty
+  ]
+
