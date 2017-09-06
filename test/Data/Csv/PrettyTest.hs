1ec78f5aa3a62d26287ddbb23ef62eb61a618e87
George Wilson <george@wils.online>
2017-09-06 13:16:58 +1000

    Consolidate test suites

--- test/Data/Csv/PrettyTest.hs
+++ test/Data/Csv/PrettyTest.hs
@@ -3,24 +3,34 @@
 
 module Data.Csv.PrettyTest (test_Pretty) where
 
-import Data.Text        (Text)
-import Test.Tasty       (TestName, TestTree, testGroup)
-import Test.Tasty.HUnit ((@?=), testCase)
-import Text.Parsec      (parse)
-import Text.Parser.Char (CharParsing)
+import Control.Lens         (view)
+import Data.Text            (Text)
+import qualified Data.Text.Lazy as Lazy
+import Data.Text.Lazy.Builder (toLazyText)
+import Data.Text1           (Text1, packed1)
+import Hedgehog             ((===), Property, Gen, forAll, property)
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Test.Tasty           (TestName, TestTree, testGroup)
+import Test.Tasty.Hedgehog  (testProperty)
+import Test.Tasty.HUnit     ((@?=), testCase)
+import Text.Parsec          (parse)
+import Text.Parser.Char     (CharParsing)
 
-import Data.Csv.Csv     (Csv (Csv))
-import Data.Csv.Field   (unspacedField)
-import Data.Csv.Parser  (comma, monoField)
-import Data.Csv.Pretty  (prettyCsv, prettyMonoField, setSeparator, textConfig)
-import Data.Csv.Record  (NonEmptyRecord (SingleFieldNER), final, noFinal)
-import Text.Quote       (Quote (SingleQuote))
+import Data.Csv.Csv         (Csv (Csv))
+import Data.Csv.Field       (unspacedField)
+import Data.Csv.Generators  (genCsv)
+import Data.Csv.Parser      (comma, monoField, separatedValues)
+import Data.Csv.Pretty      (prettyCsv, defaultConfig, prettyMonoField, setSeparator, textConfig)
+import Data.Csv.Record      (NonEmptyRecord (SingleFieldNER), final, noFinal)
+import Text.Quote           (Quote (SingleQuote))
 
 test_Pretty :: TestTree
 test_Pretty =
   testGroup "Pretty" [
     fieldRoundTrip
   , csvPretty
+  , csvRoundTrip
   ]
 
 prettyAfterParseRoundTrip :: (forall m. CharParsing m  => m a) -> (a -> Text) -> TestName -> Text -> TestTree
@@ -58,4 +68,25 @@ csvPretty =
       let subject = Csv comma mempty (final (SingleFieldNER (unspacedField SingleQuote "")))
       in pretty subject @?= "''"
   ]
+
+csvRoundTrip :: TestTree
+csvRoundTrip = testProperty "roundtrip" prop_csvRoundTrip
+
+prop_csvRoundTrip :: Property
+prop_csvRoundTrip =
+  let genSpace = Gen.text (Range.linear 0 5) (Gen.element [' ', '\t'])
+      genText  = Gen.text (Range.linear 0 100) Gen.alphaNum
+      genNonEmptyString = Gen.nonEmpty (Range.linear 0 100) Gen.alphaNum
+      genText1 = view packed1 <$> genNonEmptyString
+      gen = genCsv (pure ',') genSpace genText1 genText
+      pretty = toLazyText . prettyCsv defaultConfig
+      parseCsv :: (Monad m, CharParsing m) => m (Csv Text Text1 Text)
+      parseCsv = separatedValues ','
+  in  parsePretty parseCsv pretty gen
+
+parsePretty :: (Eq a, Show a) => (forall m. (Monad m, CharParsing m) => m a) -> (a -> Lazy.Text) -> Gen a -> Property
+parsePretty parser pretty genA =
+  property $ do
+    c <- forAll genA
+    parse parser "" (pretty c) === pure c
 
