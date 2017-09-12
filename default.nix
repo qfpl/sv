94cc0aa4b3148bc347dbb5adc5e80ac0c57cb56d
Tony Morris <tmorris@tmorris.net>
2017-09-12 10:39:46 +1000

    LICENSE -> LICENCE

--- default.nix
+++ default.nix
@@ -15,12 +15,20 @@ let
       rev = "0db19e31c20b8456c06163338d9b94841faa4049";
       sha256 = "1hil4kqknc6qmd5a6nlxdb6yfm5l7rf1pqdjkl6m0bxz6d6r5za5";
     };
+    
+    tasty-hedgehog = pkgs.fetchFromGitHub {
+      owner = "qfpl";
+      repo = "tasty-hedgehog";
+      rev = "0.1.0.1";
+      sha256 = "04pmr9q70gakd327sywpxr7qp8jnl3b0y2sqxxxcj6zj2q45q38m";
+    };
   };
 
   modifiedHaskellPackages = haskellPackages.override {
     overrides = self: super: {
       parsers = pkgs.haskell.lib.dontCheck super.parsers;
       separated = import sources.separated { inherit nixpkgs compiler; };
+      tasty-hedgehog = import sources.tasty-hedgehog {};   
     };
   };
 
--- sv.nix
+++ sv.nix
@@ -1,17 +1,18 @@
-{ mkDerivation, base, bifunctors, charset, hedgehog, lens, parsec, parsers, semigroupoids
-, semigroups, separated, stdenv, tasty, tasty-hunit
-, tasty-quickcheck, text, text1
+{ mkDerivation, base, bifunctors, charset, hedgehog, lens, parsec
+, parsers, QuickCheck, semigroupoids, semigroups, separated, stdenv
+, tasty, tasty-hedgehog, tasty-hunit, tasty-quickcheck, text, text1
 }:
 mkDerivation {
   pname = "sv";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    base bifunctors charset lens parsers semigroupoids semigroups separated text text1
+    base bifunctors charset lens parsers semigroupoids semigroups
+    separated text text1
   ];
   testHaskellDepends = [
-    base bifunctors hedgehog lens parsec parsers semigroups separated tasty tasty-hunit
-    tasty-quickcheck text text1
+    base hedgehog lens parsec parsers QuickCheck semigroups separated
+    tasty tasty-hedgehog tasty-hunit tasty-quickcheck text text1
   ];
   homepage = "https://github.com/qfpl/sv";
   description = "Values which are separated, often by commas";
