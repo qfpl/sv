495b2fe49220e3a445fd94498a935381deb1c5a3
George Wilson <george@wils.online>
2017-10-20 14:53:41 +1000

    More random scribbling

--- src/Data/Csv/Decoder.hs
+++ src/Data/Csv/Decoder.hs
@@ -1,6 +1,7 @@
 module Data.Csv.Decoder where
 
 import Control.Applicative (Applicative (pure, (<*>)), liftA2)
+import Data.Csv.Field (Field)
 import Data.Csv.Record (Record)
 import Data.Functor.Alt (Alt ((<!>)))
 import Data.Functor.Apply (Apply ((<.>)))
@@ -9,7 +10,13 @@ import Data.Semigroup (Semigroup)
 import Data.Validation (AccValidation)
 
 newtype Decode e s a =
-  Decode (Record s -> AccValidation e a)
+  Decode { runDecode :: s -> AccValidation e a }
+
+newtype FieldDecode e s1 s2 a =
+  FieldDecode { runFieldDecode :: Decode e (Field s1 s2) a }
+
+newtype RowDecode e s a =
+  RowDecode { runRowDecode :: Decode e (Record s) a }
 
 instance Functor (Decode e s) where
   fmap f (Decode g) = Decode (fmap (fmap f) g)
@@ -25,5 +32,21 @@ instance Alt (Decode e s) where
   Decode f <!> Decode g = Decode (liftA2 (<!>) f g)
 
 instance Profunctor (Decode e) where
-  dimap f g (Decode d) = Decode (dimap (fmap f) (fmap g) d)
+  dimap f g (Decode d) = Decode (dimap f (fmap g) d)
+
+instance Functor (RowDecode e s) where
+  fmap f = RowDecode . fmap f . runRowDecode
+
+instance Semigroup e => Apply (RowDecode e s) where
+  RowDecode f <.> RowDecode a = RowDecode (f <.> a)
+
+instance Semigroup e => Applicative (RowDecode e s) where
+  pure = RowDecode . pure
+  (<*>) = (<.>)
+
+instance Alt (RowDecode e s) where
+  RowDecode f <!> RowDecode g = RowDecode (f <!> g)
+
+instance Profunctor (RowDecode e) where
+  dimap f g (RowDecode d) = RowDecode (dimap (fmap f) g d)
 
