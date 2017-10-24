e1b6e28b9e3f8a43f9fe87ace3d4c0a1a564f3a8
George Wilson <george@wils.online>
2017-10-25 09:47:45 +1000

    Add hectic Alternative instance for FieldDecode. This needs serious testing

--- src/Data/Csv/Decode.hs
+++ src/Data/Csv/Decode.hs
@@ -1,79 +1,77 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TupleSections #-}
 
 module Data.Csv.Decode where
 
-import Control.Applicative (Applicative (pure, (<*>)), liftA2)
+import Control.Applicative (Applicative, Alternative, liftA2)
 import Control.Lens (view)
+import Control.Monad.Reader (ReaderT (ReaderT, runReaderT), withReaderT)
+import Control.Monad.State (MonadState, State, runState, state)
 import Data.Csv.Field (Field)
 import Data.Csv.Record (Record)
 import Data.Functor.Alt (Alt ((<!>)))
 import Data.Functor.Apply (Apply ((<.>)))
+import Data.Functor.Compose (Compose (Compose, getCompose))
+import Data.Functor.Identity (Identity (Identity, runIdentity))
 import Data.Profunctor (Profunctor (dimap))
 import Data.Semigroup (Semigroup)
 import Data.Validation (AccValidation (AccSuccess, AccFailure), _AccValidation, Validate)
 
-newtype Decode e s a =
-  Decode { runDecode :: s -> AccValidation e a }
+type Decode = DecodeT Identity
+
+newtype DecodeT f e s a =
+  DecodeT { runDecodeT :: ReaderT s (Compose f (AccValidation e)) a }
+  deriving (Functor, Applicative)
+
+instance (Apply f, Semigroup e) => Apply (DecodeT f e s) where
+  DecodeT f <.> DecodeT a = DecodeT (f <.> a)
+
+instance Functor f => Profunctor (DecodeT f e) where
+  dimap f g (DecodeT d) = DecodeT (withReaderT f (fmap g d))
+
+runDecode :: Decode e s a -> s -> AccValidation e a
+runDecode (DecodeT r) = fmap (runIdentity . getCompose) (runReaderT r)
+
 
 newtype FieldDecode e s a =
-  FieldDecode { runFieldDecode :: Decode e (Field s) a }
+  FieldDecode { runFieldDecode :: DecodeT (DecodeState s) e (Field s) a }
+  deriving (Functor, Apply, Applicative)
 
-newtype RowDecode e s a =
-  RowDecode { runRowDecode :: Decode e (Record s) a }
+instance Alt (FieldDecode e s) where
+  FieldDecode (DecodeT a) <!> FieldDecode (DecodeT b) =
+    FieldDecode . DecodeT . ReaderT $ \s -> Compose $
+      let as = getCompose (runReaderT a s)
+          bs = getCompose (runReaderT b s)
+      in  decodeState $ \fs ->
+            case runDecodeState as fs of
+              (a, gs) -> case runDecodeState bs fs of
+                (b, hs) ->
+                  let a' = fmap (,gs) a
+                      b' = fmap (,hs) b
+                  in  case a' <!> b' of
+                        AccFailure e -> (AccFailure e, hs)
+                        AccSuccess (z,js) -> (AccSuccess z, js)
 
-instance Functor (Decode e s) where
-  fmap f (Decode g) = Decode (fmap (fmap f) g)
+decoder :: (Applicative f, Validate v) => (s -> v e a) -> DecodeT f e s a
+decoder = DecodeT . ReaderT . fmap (Compose . pure . view _AccValidation)
 
-instance Semigroup e => Apply (Decode e s) where
-  Decode f <.> Decode a = Decode (liftA2 ((<.>)) f a)
-
-instance Semigroup e => Applicative (Decode e s) where
-  pure = Decode . pure . pure
-  (<*>) = (<.>)
-
-instance Alt (Decode e s) where
-  Decode f <!> Decode g = Decode (liftA2 (<!>) f g)
-
-instance Profunctor (Decode e) where
-  dimap f g (Decode d) = Decode (dimap f (fmap g) d)
-
-instance Functor (RowDecode e s) where
-  fmap f = RowDecode . fmap f . runRowDecode
-
-instance Semigroup e => Apply (RowDecode e s) where
-  RowDecode f <.> RowDecode a = RowDecode (f <.> a)
-
-instance Semigroup e => Applicative (RowDecode e s) where
-  pure = RowDecode . pure
-  (<*>) = (<.>)
-
-instance Alt (RowDecode e s) where
-  RowDecode f <!> RowDecode g = RowDecode (f <!> g)
-
-instance Profunctor (RowDecode e) where
-  dimap f g (RowDecode d) = RowDecode (dimap (fmap f) g d)
-
-decoder :: Validate v => (s -> v e a) -> Decode e s a
-decoder f = Decode (view _AccValidation . f)
-
-failure :: e -> Decode e s a
+failure :: Applicative f => e -> DecodeT f e s a
 failure = decoder . const . AccFailure
 
-success :: a -> Decode e s a
+success :: Applicative f => a -> DecodeT f e s a
 success = decoder . const . AccSuccess
 
-class RunDecode d s a | d -> s a where
-  run :: d -> s -> a
 
-instance RunDecode (Decode e s a) s (AccValidation e a) where
-  run = runDecode
+newtype DecodeState s a =
+  DecodeState { getDecodeState :: State [Field s] a }
+  deriving (Functor, Apply, Applicative, Monad, MonadState [Field s])
 
-instance RunDecode (FieldDecode e s a) (Field s) (AccValidation e a) where
-  run = run . runFieldDecode
+decodeState :: ([Field s] -> (a, [Field s])) -> DecodeState s a
+decodeState = DecodeState . state
 
-instance RunDecode (RowDecode e s a) (Record s) (AccValidation e a) where
-  run = run . runRowDecode
+runDecodeState :: DecodeState s a -> [Field s] -> (a, [Field s])
+runDecodeState = runState . getDecodeState
+
+drop1 :: DecodeState s ()
+drop1 = DecodeState (state $ \l -> ((), drop 1 l))
 
