1b2992abb0364164c615d031bbc726772cbf5469
George Wilson <george@wils.online>
2018-01-30 17:07:22 +1000

    Remove more unused code

--- src/Data/Functor/Compose/Extra.hs
+++ src/Data/Functor/Compose/Extra.hs
@@ -3,8 +3,6 @@
 module Data.Functor.Compose.Extra (
   lmapC
 , rmapC
-, injl
-, injr
 ) where
 
 import Data.Functor.Compose (Compose (Compose))
@@ -15,9 +13,3 @@ lmapC f (Compose fga) = Compose (f fga)
 rmapC :: Functor f => (g a -> g' a') -> Compose f g a -> Compose f g' a'
 rmapC g (Compose fga) =
   Compose (fmap g fga)
-
-injl :: (Functor f, Applicative g) => f a -> Compose f g a
-injl fa = Compose (fmap pure fa)
-
-injr :: (Applicative f) => g a -> Compose f g a
-injr ga = Compose (pure ga)
