bf8764f5156ee1c9f86875dd495b73c563511028
George Wilson <george@wils.online>
2018-02-12 15:58:31 +1000

    Add Syntax.hs module just to round things out.

---
+++ src/Data/Sv/Syntax.hs
@@ -0,0 +1,18 @@
+{-|
+Module      : Data.Sv.Syntax
+Copyright   : (C) CSIRO 2017-2018
+License     : BSD3
+Maintainer  : George Wilson <george.wilson@data61.csiro.au>
+Stability   : experimental
+Portability : non-portable
+-}
+
+module Data.Sv.Syntax (
+  module Data.Sv.Syntax.Field
+, module Data.Sv.Syntax.Record
+, module Data.Sv.Syntax.Sv
+) where
+
+import Data.Sv.Syntax.Field
+import Data.Sv.Syntax.Record
+import Data.Sv.Syntax.Sv
