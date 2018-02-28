e2e8f82b875b6d5ba0fb0e9c2ecfd3df60497ea7
George Wilson <george@wils.online>
2018-02-28 13:29:09 +1000

    Add an example to demonstrate encoding

--- examples/test/test.hs
+++ examples/test/test.hs
@@ -1,4 +1,5 @@
 import qualified Data.Sv.Example.Concat as Concat
+import qualified Data.Sv.Example.Encoding as Encoding
 import qualified Data.Sv.Example.Numbers as Numbers
 import qualified Data.Sv.Example.Ragged as Ragged
 import qualified Data.Sv.Example.Requote as Requote
@@ -8,6 +9,7 @@ import qualified Data.Sv.Example.TableTennis as TableTennis
 main :: IO ()
 main = do
   Concat.main
+  Encoding.main
   Numbers.main
   Ragged.main
   Requote.main
