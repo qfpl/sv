ce417c0ea666717ff95eb45709eea0f1778d9c67
George Wilson <george@wils.online>
2018-03-20 13:46:12 +1000

    Clean up Requote example

--- examples/src/Data/Sv/Example/Requote.hs
+++ examples/src/Data/Sv/Example/Requote.hs
@@ -4,13 +4,14 @@ import Control.Lens
 import Control.Monad (unless)
 import Data.ByteString (ByteString)
 import System.Exit (exitFailure)
-import qualified Text.Trifecta as T (parseFromFile)
 
 import Data.Sv
-import Data.Sv.Parse (separatedValues)
 import Data.Sv.Print (writeSvToFile)
 import Text.Escape (Unescaped (Unescaped))
 import Text.Quote (Quote (DoubleQuote))
+
+-- Manipulates the syntax directly with optics without decoding to data types
+-- Rewrites a file with inconsistent quoting to consistently use double quotes
 
 original :: FilePath
 original = "csv/requote.csv"
@@ -21,29 +22,8 @@ fixed = "csv/requote.fixed.csv"
 golden :: FilePath
 golden = "csv/requote.golden.csv"
 
-main :: IO ()
-main = do
-  requote
-  expected <- readFile golden
-  actual <- readFile fixed
-  unless (expected == actual)
-    exitFailure
-
--- Manipulates the syntax directly with optics without decoding to data types
--- Rewrites a file with inconsistent quoting to consistently use double quotes
-
 opts :: ParseOptions ByteString
 opts = defaultParseOptions
-
-requote :: IO ()
-requote = do
-  svMay <- T.parseFromFile (separatedValues opts) original
-  case svMay of
-    Nothing -> exitFailure
-    Just s ->
-      let s' :: Sv ByteString
-          s' = fixQuotes s
-      in  writeSvToFile fixed s'
 
 fixQuotes :: Sv s -> Sv s
 fixQuotes = over headerFields fixQuote . over recordFields fixQuote
@@ -54,3 +34,21 @@ fixQuotes = over headerFields fixQuote . over recordFields fixQuote
       Quoted _ v -> Quoted DoubleQuote v
     headerFields = traverseHeader . fields
     recordFields = traverseRecords . fields
+
+main :: IO ()
+main = do
+  requote
+  expected <- readFile golden
+  actual <- readFile fixed
+  unless (expected == actual)
+    exitFailure
+
+requote :: IO ()
+requote = do
+  svEither <- parseSvFromFile opts original
+  case svEither of
+    Left _ -> exitFailure
+    Right s ->
+      let s' :: Sv ByteString
+          s' = fixQuotes s
+      in  writeSvToFile fixed s'
