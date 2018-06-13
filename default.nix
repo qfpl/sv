31460db015fcb15a6009c242f6d647a128e78a57
George Wilson <george@wils.online>
2018-06-13 16:59:43 +1000

    Fix the nix build

--- default.nix
+++ default.nix
@@ -26,10 +26,58 @@ let
     hedgehog = pkgs.fetchFromGitHub {
       owner  = "hedgehogqa";
       repo   = "haskell-hedgehog";
-      rev    = "7858d626b198621bc674fbc235c7980fb4002f78";
-      sha256 = "0mmypd6f3imh7bk6br9m9aj97k2yibz2bqcw3a5svp962zsjbkyp";
+      rev    = "0.6";
+      sha256 = "101bxgnxdmjg6x5jdjgbzayb747lxv8yv28bjg0kr6xw4kqi8kpw";
     };
 
+    tasty-hedgehog = pkgs.fetchFromGitHub {
+      owner  = "qfpl";
+      repo   = "tasty-hedgehog";
+      rev    = "9797ca980e547c160b5e9e3f07d7b0d1d5c40fee";
+      sha256 = "039r8hay6cyq762ajn89nj4bfgz50br15x4nkracw3kzdyikn5xh";
+    };
+
+    hw-dsv = pkgs.fetchFromGitHub {
+      owner  = "haskell-works";
+      repo   = "hw-dsv";
+      rev    = "7c3606379bcd7b371542d938b9847a7c753fdf11";
+      sha256 = "1447p30mclwd083p3r701nsvhv0mg75nr278nrxi14ymh8g298xr";
+    };
+
+    hw-hspec-hedgehog = pkgs.fetchFromGitHub {
+      owner  = "haskell-works";
+      repo   = "hw-hspec-hedgehog";
+      rev    = "v0.1.0.5";
+      sha256 = "1hmpa31j29nk0yh2xs5lcbdlj7rpyyimagmwk80b8qbj4kmpzyzs";
+    };
+
+    hw-prim = pkgs.fetchFromGitHub {
+      owner  = "haskell-works";
+      repo   = "hw-prim";
+      rev    = "v0.6.2.0";
+      sha256 = "02m76y4h75gsm26nsc0xy88c39k75by5dhp9gmqqm1fkdy0p5c3n";
+    };
+
+    hw-rankselect = pkgs.fetchFromGitHub {
+      owner  = "haskell-works";
+      repo   = "hw-rankselect";
+      rev    = "v0.12.0.3";
+      sha256 = "0l14300yjy6b5p27f1x6iljsq5ph874wnzfz0wszby8cg3r918xh";
+    };
+
+    hw-rankselect-base = pkgs.fetchFromGitHub {
+      owner  = "haskell-works";
+      repo   = "hw-rankselect-base";
+      rev    = "v0.3.2.0";
+      sha256 = "183czzmhwb9l0v9cw0g50dzph87n63lz08lhk7hqj636d1ks6ng9";
+    };
+
+    bits-extra = pkgs.fetchFromGitHub {
+      owner  = "haskell-works";
+      repo   = "bits-extra";
+      rev    = "897d61bb5ae629bf7480b84222c2e9a71c03121b";
+      sha256 = "0l6zs9pv6hnsfsinf8zhayjcygdqc0l73zsmbhy23jp20hdr2i1n";
+    };
   };
 
   modifiedHaskellPackages = haskellPackages.override {
@@ -38,6 +86,14 @@ let
       separated = pkgs.haskell.lib.dontCheck (import sources.separated { inherit nixpkgs compiler; });
       validation= import sources.validation { inherit nixpkgs compiler; };
       hedgehog = super.callCabal2nix "hedgehog" "${sources.hedgehog}/hedgehog" {};
+      tasty-hedgehog = super.callCabal2nix "tasty-hedgehog" "${sources.tasty-hedgehog}" {};
+      hw-dsv = super.callCabal2nix "hw-dsv" "${sources.hw-dsv}" {};
+      hw-hspec-hedgehog = super.callCabal2nix "hw-hspec-hedgehog" "${sources.hw-hspec-hedgehog}" {};
+      hw-prim = super.callCabal2nix "hw-prim" "${sources.hw-prim}" {};
+      hw-rankselect = pkgs.haskell.lib.dontCheck (super.callCabal2nix "hw-rankselect" "${sources.hw-rankselect}" {});
+      hw-rankselect-base = super.callCabal2nix "hw-rankselect-base" "${sources.hw-rankselect-base}" {};
+      bits-extra = super.callCabal2nix "bits-extra" "${sources.bits-extra}" {};
+      sv-common = super.callCabal2nix "sv-common" ./sv-common {};
     };
   };
 
--- sv.nix
+++ sv.nix
@@ -1,23 +1,23 @@
-{ mkDerivation, ansi-wl-pprint, attoparsec, base, bifunctors
-, bytestring, charset, containers, contravariant, deepseq, hedgehog
-, lens, mtl, parsec, parsers, profunctors, readable, semigroupoids
-, semigroups, stdenv, tasty, tasty-hedgehog, tasty-hunit, text
+{ mkDerivation, attoparsec, base, bifunctors, bytestring
+, containers, contravariant, deepseq, hedgehog, hw-dsv, lens, mtl
+, parsec, parsers, profunctors, readable, semigroupoids, semigroups
+, stdenv, sv-common, tasty, tasty-hedgehog, tasty-hunit, text
 , transformers, trifecta, utf8-string, validation, vector, void
 }:
 mkDerivation {
   pname = "sv";
-  version = "0.1";
+  version = "0.2";
   src = ./.;
   libraryHaskellDepends = [
-    ansi-wl-pprint attoparsec base bifunctors bytestring charset
-    containers contravariant deepseq lens mtl parsec parsers
-    profunctors readable semigroupoids semigroups text transformers
-    trifecta utf8-string validation vector void
+    attoparsec base bifunctors bytestring containers contravariant
+    deepseq hw-dsv lens mtl parsec profunctors readable semigroupoids
+    semigroups sv-common text transformers trifecta utf8-string
+    validation vector void
   ];
   testHaskellDepends = [
-    ansi-wl-pprint base bytestring contravariant hedgehog lens parsers
-    semigroupoids semigroups tasty tasty-hedgehog tasty-hunit text
-    trifecta utf8-string validation vector
+    base bytestring contravariant hedgehog lens parsers semigroupoids
+    semigroups tasty tasty-hedgehog tasty-hunit text trifecta
+    utf8-string validation vector
   ];
   homepage = "https://github.com/qfpl/sv";
   description = "Encode and decode separated values (CSV, PSV, ...)";
