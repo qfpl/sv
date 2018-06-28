5668a1ab3f231d172b3a23187013a4aa83e4813b
George Wilson <george@wils.online>
2018-06-28 14:34:02 +1000

    Test decoders agree with cassava

--- test/tasty.hs
+++ test/tasty.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Test.Tasty (defaultMain, testGroup)
 
+import Data.Sv.CassavaTest (test_CassavaAgreement)
 import Data.Sv.DecodeTest (test_Decode)
 import Data.Sv.EncodeTest (test_Encode)
 import Data.Sv.RoundTripsDecodeEncode (test_Roundtrips)
@@ -12,4 +13,5 @@ main =
     test_Decode
   , test_Encode
   , test_Roundtrips
+  , test_CassavaAgreement
   ]
