008fbc37cd6bef2d3f7185335ed1d71d904938b7
George Wilson <george@wils.online>
2018-06-29 16:16:59 +1000

    Move svfactor to its own repository

--- svfactor/changelog.md
+++
@@ -1,5 +0,0 @@
-# Revision history for sv
-
-## 0.1 -- 2018-03-06
-
-* First version. Released on an unsuspecting world.
--- svfactor/examples/test/test.hs
+++
@@ -1,7 +0,0 @@
-import qualified Data.Svfactor.Example.Concat as Concat
-import qualified Data.Svfactor.Example.Requote as Requote
-
-main :: IO ()
-main = do
-  Concat.main
-  Requote.main
