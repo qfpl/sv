db9deab484ad111443a98145d2874c9b9ace8a41
George Wilson <george@wils.online>
2018-07-12 11:47:35 +1000

    Update nix build

--- sv.nix
+++ sv.nix
@@ -1,18 +1,16 @@
 { mkDerivation, attoparsec, base, bifunctors, bytestring, cassava
-, containers, contravariant, deepseq, hedgehog, hw-dsv, lens, mtl
-, Only, parsec, parsers, profunctors, readable, semigroupoids
-, semigroups, stdenv, tasty, tasty-hedgehog, tasty-hunit, text
-, transformers, trifecta, utf8-string, validation, vector, void
+, contravariant, hedgehog, hw-dsv, lens, Only, parsers
+, semigroupoids, semigroups, stdenv, sv-core, tasty, tasty-hedgehog
+, tasty-hunit, text, transformers, trifecta, utf8-string
+, validation, vector
 }:
 mkDerivation {
   pname = "sv";
   version = "0.2";
   src = ./.;
   libraryHaskellDepends = [
-    attoparsec base bifunctors bytestring containers contravariant
-    deepseq hw-dsv lens mtl parsec profunctors readable semigroupoids
-    semigroups text transformers trifecta utf8-string validation vector
-    void
+    attoparsec base bifunctors bytestring contravariant hw-dsv
+    semigroupoids sv-core transformers utf8-string validation
   ];
   testHaskellDepends = [
     base bytestring cassava contravariant hedgehog lens Only parsers
