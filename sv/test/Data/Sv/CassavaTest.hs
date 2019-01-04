866b823d914d2c857570b451b8b569e95aa59f5d
George Wilson <george@wils.online>
2019-01-04 16:53:18 +1000

    Fix random test failure due to cassava not roundtripping

--- sv/test/Data/Sv/CassavaTest.hs
+++ sv/test/Data/Sv/CassavaTest.hs
@@ -4,12 +4,14 @@ module Data.Sv.CassavaTest (test_CassavaAgreement) where
 
 import qualified Data.ByteString as BS
 import qualified Data.Csv as Csv
-import Data.Sv (Validation (Failure, Success), Headedness (Unheaded), ParseOptions (_headedness), defaultParseOptions)
-import qualified Data.Sv as Sv (parseDecode)
+import Data.Sv (Validation (Failure, Success), Headedness (Unheaded), ParseOptions (_headedness), defaultParseOptions, defaultEncodeOptions)
+import qualified Data.Sv as Sv (parseDecode, encode)
 import Data.Sv.Decode (Decode')
 import qualified Data.Sv.Decode as Sv
+import Data.Sv.Encode (Encode)
+import qualified Data.Sv.Encode as E
 import Data.Vector as V
-import Data.Tuple.Only (Only (Only, fromOnly))
+import Data.Tuple.Only (Only (fromOnly))
 import Hedgehog (Gen, (===), failure, forAll, property)
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
@@ -19,24 +21,23 @@ import Test.Tasty.Hedgehog (testProperty)
 test_CassavaAgreement :: TestTree
 test_CassavaAgreement =
   testGroup "cassava agreement"
-    [ cassavaAgreement "int" Sv.int (Gen.int (Range.linear (-10000000) 10000000))
-    , cassavaAgreement "char" Sv.char (Gen.unicode)
-    , cassavaAgreement "integer" Sv.integer (Gen.integral (Range.linear (-10000000) 10000000))
-    , cassavaAgreement "string" Sv.string (Gen.string (Range.linear 1 500) Gen.unicode)
-    , cassavaAgreement "bytestring" Sv.byteString (Gen.utf8 (Range.linear 1 500) Gen.unicode)
-    , cassavaAgreement "float" Sv.float (Gen.float (Range.exponentialFloat (-10000000) 10000000))
-    , cassavaAgreement "double" Sv.double (Gen.double (Range.exponentialFloat (-10000000) 10000000))
+    [ cassavaAgreement "int" Sv.int E.int (Gen.int (Range.linear (-10000000) 10000000))
+    , cassavaAgreement "char" Sv.char E.char (Gen.unicode)
+    , cassavaAgreement "integer" Sv.integer E.integer (Gen.integral (Range.linear (-10000000) 10000000))
+    , cassavaAgreement "string" Sv.string E.string (Gen.string (Range.linear 1 500) Gen.unicode)
+    , cassavaAgreement "bytestring" Sv.byteString E.byteString (Gen.utf8 (Range.linear 1 600) Gen.unicode)
+    , cassavaAgreement "float" Sv.float E.float (Gen.float (Range.exponentialFloat (-10000000) 10000000))
+    , cassavaAgreement "double" Sv.double E.double (Gen.double (Range.exponentialFloat (-10000000) 10000000))
     ]
 
 opts :: ParseOptions
 opts = defaultParseOptions { _headedness = Unheaded }
 
 -- | Test that decoding with sv gets the same result as decoding with cassava
-cassavaAgreement :: forall a . (Csv.FromField a, Csv.ToField a, Show a, Eq a) => TestName -> Decode' BS.ByteString a -> Gen a -> TestTree
-cassavaAgreement name dec gen = testProperty name $ property $ do
+cassavaAgreement :: forall a . (Csv.FromField a, Csv.ToField a, Show a, Eq a) => TestName -> Decode' BS.ByteString a -> Encode a -> Gen a -> TestTree
+cassavaAgreement name dec enc gen = testProperty name $ property $ do
   a <- forAll gen
-  let oa = Only a
-  let sa = Csv.encode [oa]
+  let sa = Sv.encode enc defaultEncodeOptions [a]
   let cassava :: Either String [a]
       cassava = fmap fromOnly . V.toList <$> Csv.decode Csv.NoHeader sa
       sv = Sv.parseDecode dec opts sa
